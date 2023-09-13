d1acd4d23c85055f6db8b6dc80c086e8888dda2f
ATUL PATEL <connecttoatul@gmail.com>
2023-09-12 21:05:41 -0500

    Create Main.java

---
+++ ThreadUsingFixThreadPool/Main.java
@@ -0,0 +1,14 @@
+package ThreadUsingFixThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args){
+        ExecutorService th = Executors.newFixedThreadPool(10);
+        for(int i=0;i<=21;i++){
+            NumberPrinter np = new NumberPrinter(i);
+            th.execute(np);
+        }
+    }
+}
