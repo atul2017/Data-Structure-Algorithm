4c3ba52dc68d97b96783f227b1963210a45b12ff
ATUL PATEL <connecttoatul@gmail.com>
2023-10-01 15:15:07 -0500

    Create NumberPrinter.java

---
+++ Threading/ThreadUsingFixThreadPool/NumberPrinter.java
@@ -0,0 +1,12 @@
+package Threading.ThreadUsingFixThreadPool;
+
+public class NumberPrinter implements Runnable {
+    int printNumber  = 0;
+    public NumberPrinter(int num){
+        printNumber = num;
+    }
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(" This is my number: "+printNumber);
+    }
+}
