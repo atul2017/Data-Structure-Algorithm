ee616e54a08bb0f4669f401d30f741f483db78fd
ATUL PATEL <connecttoatul@gmail.com>
2023-10-09 10:45:43 -0400

    Create NumberPrinter.java

---
+++ Threading/ThreadUsingCachedThreadPool/NumberPrinter.java
@@ -0,0 +1,14 @@
+package Threading.ThreadUsingCachedThreadPool;
+
+public class NumberPrinter implements Runnable {
+
+    int number = 0;
+    public NumberPrinter(int i){
+        this.number = i;
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Print the number: "+number +" :"+Thread.currentThread().getName());
+    }
+}
